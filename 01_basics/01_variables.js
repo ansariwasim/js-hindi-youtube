36ed5baeae25ca91b92fe7fc0bde9db3c9514e11
ansariwasim <107686270+ansariwasim@users.noreply.github.com>
2022-12-26 14:43:13 +0000

    learn const,let,var,

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553;
+let accountEmail = "wasim@google.com";
+var accountPassword = "12345";
+accontCity = "Jaipur";
+let accountStage;
+
+
+
+// accountId = 2; // not allowed
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+accountEmail = "oasim@gmail.com"
+accountPassword = "111111"
+accontCity = "gaur"
+
+console.table([accountEmail,accountPassword,accontCity,accountStage]);
