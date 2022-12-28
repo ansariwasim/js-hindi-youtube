ddadaae495fcafed9168314470c4d2281ad655e0
ansariwasim <107686270+ansariwasim@users.noreply.github.com>
2022-12-28 04:32:16 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+ "use strict" // treat all js code as newer version
+
+// alert(2+4); // we are using node.js, not browser
+
+console.log(3 
+    + 3); // code readability should be high
+
+console.log("Wasim Ansari");
+
+let name = "Wasim"
+let age = 21
+let isLoggedIn = false 
+
+// number => 2 to power 53
+//  bigint 
+//  string => ""
+//  boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Wasim")
+console.log(typeof undefined); // undefined
+console.log(typeof null) // object 
+
+
